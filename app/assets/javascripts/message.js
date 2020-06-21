0fca8cb64ffe7985d3983f20039969ac108bb218
murakamitosiki <toshiki.4938.tji@gmail.com>
2020-06-21 22:46:04 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){ 
-  function buildHTML(message){
+  function buildHTML(message){//トレーに乗ったまま具材がきたので綺麗に盛り付けている
    if ( message.image ) {
      var html =
-      `<div class="main__center__message">
+      `<div class="main__center__message" data-message-id=${message.id}>
          <div class="main__center__message__info">
            <div class="main__center__message__info__user">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-     `<div class="main__center__message">
+     `<div class="main__center__message" data-message-id=${message.id}>
          <div class="main__center__message__info">
            <div class="main__center__message__info__user">
              ${message.user_name}
@@ -60,5 +60,44 @@ $('#new_message').on('submit', function(e){
   .always(function(){
     $('.main__bottom__text__send').prop('disabled', false);//連続でボタンをおす記述
   })
-})
+
+  var reloadMessages = function() {//非同期通信で使っていた物を利用して自動更新機能もつくる
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得 ノードの一番最後にくる文
+    var last_message_id = $('.main__center__message:last').data("message-id");
+    // 上で定義した5行目から20行目までを.main__center__message:lastでこここで使う
+    // ("message-id")で前にきていたお客さんのメニューを知りたい
+    console.log(last_message_id)
+    $.ajax({//厨房の誰かを指定してあげて渡す
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}//厨房にメニューを教えている。
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';//空のお盆を用意している。
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {//メニューが複数ある場合配列でまとめ、一品づつつくる
+          insertHTML += buildHTML(message)//盛り付けを上で行う。さらに上で作ったbuildHTML(message)のメニューが空のお皿(insertHTM)に入る
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);//お客様に提供する部分
+        $('.main__center').animate({ scrollTop: $('.main__center')[0].scrollHeight});
+        // 一番下にスクロールする記載
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
+});
 });
